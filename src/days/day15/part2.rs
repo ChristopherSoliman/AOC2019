49d9bbebc845f3ac3138b75e05f523487527e5ea
ChristopherSoliman <csoliman10@gmail.com>
2025-01-05 11:16:41 +0200

    day 15: complete

---
+++ src/days/day15/part2.rs
@@ -0,0 +1,350 @@
+use std::collections::HashSet;
+
+#[derive(Clone, Copy, Debug)]
+struct Parameter {
+    index: usize,
+    value: i64,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum ParameterType {
+    Position,
+    Immediate,
+    Relative,
+}
+
+impl ParameterType {
+    pub fn from_int(instruction: u32) -> [ParameterType; 3] {
+        let mut val = instruction.clone();
+        let mut i = 2;
+        let mut types: [ParameterType; 3] = [ParameterType::Position; 3];
+        while i >= 0 && instruction > 0 {
+            let temp = val % 10;
+            match temp {
+                0 => types[(2 - i) as usize] = ParameterType::Position,
+                1 => types[(2 - i) as usize] = ParameterType::Immediate,
+                2 => types[(2 - i) as usize] = ParameterType::Relative,
+                _ => panic!("invalid parameter mode"),
+            }
+            val /= 10;
+            i -= 1;
+        }
+        types
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Operation {
+    Add,
+    Multiply,
+    Input,
+    Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
+    AdjustRelativeBase,
+}
+
+impl Operation {
+    pub fn from_int(opcode: u32) -> Self {
+        match opcode {
+            1 => Operation::Add,
+            2 => Operation::Multiply,
+            3 => Operation::Input,
+            4 => Operation::Output,
+            5 => Operation::JumpIfTrue,
+            6 => Operation::JumpIfFalse,
+            7 => Operation::LessThan,
+            8 => Operation::Equals,
+            9 => Operation::AdjustRelativeBase,
+            _ => panic!("invalid opcode"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Opcode {
+    parameter_modes: [ParameterType; 3],
+    opcode: Operation,
+    variables: u32,
+}
+
+impl Opcode {
+    pub fn from_int(instruction: u32) -> Self {
+        let opcode = Operation::from_int((instruction % 100) as u32);
+        let variables = match opcode {
+            Operation::Add | Operation::Multiply | Operation::LessThan | Operation::Equals => 3,
+            Operation::JumpIfTrue | Operation::JumpIfFalse => 2,
+            Operation::Input | Operation::Output | Operation::AdjustRelativeBase => 1,
+        };
+        let parameter_modes = ParameterType::from_int(instruction / 100);
+        Self {
+            parameter_modes,
+            opcode,
+            variables,
+        }
+    }
+
+    pub fn get_params(&self, intcode: &Vec<i64>, ipointer: &usize, base: &i64) -> Vec<Parameter> {
+        let mut vars: Vec<Parameter> = vec![];
+        for i in 0..self.variables as usize {
+            match self.parameter_modes[i] {
+                ParameterType::Position => {
+                    let idx = intcode[ipointer + i + 1];
+                    if idx < 0 {
+                        panic!("attempted to access negative index");
+                    }
+                    if idx >= intcode.len() as i64 {
+                        vars.push(Parameter {
+                            index: idx as usize,
+                            value: 0,
+                        });
+                    } else {
+                        vars.push(Parameter {
+                            index: idx as usize,
+                            value: intcode[idx as usize],
+                        });
+                    }
+                }
+                ParameterType::Immediate => {
+                    vars.push(Parameter {
+                        index: ipointer + i + 1,
+                        value: intcode[ipointer + i + 1],
+                    });
+                }
+                ParameterType::Relative => {
+                    let idx = intcode[ipointer + i + 1] + base;
+                    if idx < 0 {
+                        panic!("attempted to access negative index");
+                    }
+                    if idx >= intcode.len() as i64 {
+                        vars.push(Parameter {
+                            index: idx as usize,
+                            value: 0,
+                        });
+                    } else {
+                        vars.push(Parameter {
+                            index: idx as usize,
+                            value: intcode[idx as usize],
+                        });
+                    }
+                }
+            };
+        }
+        vars
+    }
+}
+
+struct Program {
+    intcode: Vec<i64>,
+    ipointer: usize,
+    inputs: Vec<i32>,
+    relative_base: i64,
+}
+
+impl Program {
+    pub fn new(intcode: Vec<i64>, inputs: Vec<i32>) -> Self {
+        Self {
+            intcode,
+            ipointer: 0,
+            relative_base: 0,
+            inputs,
+        }
+    }
+
+    pub fn get_inputs(&mut self) -> &mut Vec<i32> {
+        &mut self.inputs
+    }
+
+    fn fill_empty(&mut self, idx: &usize) {
+        for _ in self.intcode.len()..=*idx {
+            self.intcode.push(0);
+        }
+    }
+
+    pub fn execute(&mut self) -> Option<i64> {
+        while self.intcode[self.ipointer] != 99 {
+            let opcode = Opcode::from_int(self.intcode[self.ipointer] as u32);
+            match opcode.opcode {
+                Operation::Add => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    self.fill_empty(&params[2].index);
+                    self.intcode[params[2].index] = params[0].value + params[1].value
+                }
+                Operation::Multiply => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    self.fill_empty(&params[2].index);
+                    self.intcode[params[2].index] = params[0].value * params[1].value;
+                }
+                Operation::Input => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    self.intcode[params[0].index] = self.inputs.remove(0) as i64;
+                }
+                Operation::Output => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    self.ipointer += opcode.variables as usize + 1;
+                    return Some(params[0].value);
+                }
+                Operation::JumpIfTrue => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    if params[0].value != 0 {
+                        self.ipointer = params[1].value as usize;
+                        continue;
+                    }
+                }
+                Operation::JumpIfFalse => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    if params[0].value == 0 {
+                        self.ipointer = params[1].value as usize;
+                        continue;
+                    }
+                }
+                Operation::LessThan => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    let val = if params[0].value < params[1].value {
+                        1
+                    } else {
+                        0
+                    };
+                    self.fill_empty(&params[2].index);
+                    self.intcode[params[2].index] = val;
+                }
+                Operation::Equals => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    let val = if params[0].value == params[1].value {
+                        1
+                    } else {
+                        0
+                    };
+                    self.fill_empty(&params[2].index);
+                    self.intcode[params[2].index] = val;
+                }
+                Operation::AdjustRelativeBase => {
+                    let params =
+                        opcode.get_params(&self.intcode, &self.ipointer, &self.relative_base);
+                    self.relative_base += params[0].value;
+                }
+            }
+
+            self.ipointer += opcode.variables as usize + 1;
+        }
+        None
+    }
+}
+
+pub fn part2(path: &str) -> u32 {
+    let intcode = std::fs::read_to_string(path)
+        .expect("File should be there")
+        .lines()
+        .flat_map(|l| {
+            l.split(",")
+                .map(|n| n.parse::<i64>().expect("Failed to parse int"))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let ox_dirs = find_ox_path(&intcode);
+
+    let mut seen: HashSet<(i32, i32)> = HashSet::new();
+    let mut q: Vec<(Vec<i32>, u32)> = vec![];
+    q.push((ox_dirs, 0));
+    let mut max_time = 0;
+
+    while !q.is_empty() {
+        let (dirs, time) = q.remove(0);
+        if time > max_time {
+            max_time = time;
+        }
+        let mut pos = (0, 0);
+        let mut program = Program::new(intcode.clone(), dirs.clone());
+        for dir in &dirs {
+            match dir {
+                1 => pos.1 += 1,
+                2 => pos.1 -= 1,
+                3 => pos.0 -= 1,
+                4 => pos.0 += 1,
+                _ => unreachable!("invalid direction"),
+            }
+            program.execute();
+        }
+
+        seen.insert(pos);
+        for i in 1..=4 {
+            let mut new_pos = pos.clone();
+            match i {
+                1 => new_pos.1 += 1,
+                2 => new_pos.1 -= 1,
+                3 => new_pos.0 -= 1,
+                4 => new_pos.0 += 1,
+                _ => unreachable!("invalid direction"),
+            }
+            if seen.contains(&new_pos) {
+                continue;
+            }
+
+            let mut new_dirs = dirs.clone();
+
+            program.get_inputs().push(i as i32);
+            let status_code = program.execute().unwrap();
+
+            if status_code != 0 {
+                new_dirs.push(i as i32);
+                q.push((new_dirs, time + 1));
+                program.get_inputs().push(opposite(i as u8) as i32);
+                program.execute().unwrap();
+            }
+        }
+    }
+    max_time
+}
+
+fn find_ox_path(intcode: &Vec<i64>) -> Vec<i32> {
+    let mut q: Vec<Vec<i32>> = vec![];
+    q.push(vec![]);
+
+    while !q.is_empty() {
+        let dirs = q.remove(0);
+        let mut program = Program::new(intcode.clone(), dirs.clone());
+        for _ in 0..dirs.len() {
+            if program.execute().unwrap() == 2 {
+                return dirs;
+            }
+        }
+        for i in 1..=4 {
+            if !dirs.is_empty() && opposite(i) == *dirs.last().unwrap() as u8 {
+                continue;
+            }
+            let mut new_dirs = dirs.clone();
+
+            program.get_inputs().push(i as i32);
+            let status_code = program.execute().unwrap();
+
+            if status_code != 0 {
+                new_dirs.push(i as i32);
+                q.push(new_dirs);
+                program.get_inputs().push(opposite(i as u8) as i32);
+                program.execute().unwrap();
+            }
+        }
+    }
+    panic!("no solution found");
+}
+
+fn opposite(dir: u8) -> u8 {
+    match dir {
+        1 => 2,
+        2 => 1,
+        3 => 4,
+        4 => 3,
+        _ => unreachable!("invalid direction"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,6 @@ fn main() {
     //day11::run();
     //day12::run();
     //day13::run();
-    day14::run();
+    //day14::run();
+    day15::run();
 }
