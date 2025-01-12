4e721f09c144c37cf532479adb6c2f9b3cbdebc9
ChristopherSoliman <csoliman10@gmail.com>
2025-01-12 14:48:49 +0200

    day22: complete

--- src/days/day22/part2.rs
+++ src/days/day22/part2.rs
@@ -1,18 +1,14 @@
-use core::panic;
-
 #[derive(Debug)]
 enum Shuffle {
     NewStack,
     Cut(i32),
-    Increment(i32),
+    Increment(u64),
 }
 
-//const DECK_SIZE: usize = 119315717514047;
-//const REPS: usize = 101741582076661;
-const DECK_SIZE: usize = 10007;
-const REPS: usize = 1;
+const DECK_SIZE: u64 = 119315717514047;
+const REPS: u64 = 101741582076661;
 
-pub fn part2(path: &str) -> usize {
+pub fn part2(path: &str) -> u64 {
     let input = std::fs::read_to_string(path).expect("File should be there");
 
     let shuffles = input
@@ -28,42 +24,83 @@ pub fn part2(path: &str) -> usize {
             Shuffle::NewStack
         })
         .collect::<Vec<_>>();
-    let mut deck: Vec<usize> = vec![];
-    for i in 0..DECK_SIZE {
-        deck.push(i);
-    }
-    let mut i = 0;
-    while i < REPS {
-        for op in &shuffles {
-            deck = shuffle(&deck, &op);
-        }
-        i += 1;
-    }
 
-    for i in 0..DECK_SIZE {
-        if deck[i] == 2019 {
-            return i;
-        }
-    }
-    panic!("No solution found");
+    let x = 2020;
+    let y = call_reverse(x, &shuffles);
+    let z = call_reverse(y, &shuffles);
+
+    let a = ((z as i128 + DECK_SIZE as i128 - y as i128)
+        * mod_inv(y + DECK_SIZE - x, DECK_SIZE) as i128
+        % DECK_SIZE as i128) as u64;
+    let b = ((y as i128 - (a as i128 * x as i128)) % DECK_SIZE as i128) as i64;
+
+    let a_pow = mod_pow(a, REPS, DECK_SIZE);
+    let mut v =
+        (a_pow as i128 - 1_i128) * mod_inv(a as u64 - 1, DECK_SIZE) as i128 % DECK_SIZE as i128;
+    v = (v as i128 * b as i128) % DECK_SIZE as i128;
+    v = (v as i128 + a_pow as i128 * x as i128) % DECK_SIZE as i128;
+    (v % DECK_SIZE as i128) as u64
 }
 
-fn shuffle(deck: &Vec<usize>, shuffle_type: &Shuffle) -> Vec<usize> {
-    let mut new_deck = deck.clone();
+fn mod_pow(base: u64, exp: u64, modulo: u64) -> u64 {
+    if modulo == 1 {
+        return 0;
+    }
+    let mut exp = exp;
+    let mut base = base % modulo;
+    let mut result = 1;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = ((result as u128 * base as u128) % modulo as u128) as u64;
+        }
+        exp >>= 1;
+        base = ((base as u128 * base as u128) % modulo as u128) as u64;
+    }
+    result
+}
+
+fn call_reverse(i: u64, shuffles: &Vec<Shuffle>) -> u64 {
+    let mut y = i;
+    for op in shuffles.iter().rev() {
+        y = new_idx(y, &op);
+    }
+    y
+}
+
+fn new_idx(idx: u64, shuffle_type: &Shuffle) -> u64 {
     match shuffle_type {
-        Shuffle::NewStack => {
-            new_deck.reverse();
-        }
-        Shuffle::Cut(n) => {
-            for i in 0..DECK_SIZE {
-                new_deck[((DECK_SIZE as i32 - n) as usize + i) % DECK_SIZE] = deck[i];
-            }
-        }
+        Shuffle::NewStack => DECK_SIZE - 1 - idx,
+        Shuffle::Cut(n) => (idx as i64 + *n as i64).rem_euclid(DECK_SIZE as i64) as u64,
         Shuffle::Increment(n) => {
-            for i in 0..DECK_SIZE {
-                new_deck[(i * *n as usize) % DECK_SIZE] = deck[i];
-            }
+            return (mod_inv(*n, DECK_SIZE) as u128 * idx as u128 % DECK_SIZE as u128) as u64;
         }
     }
-    new_deck
+}
+
+fn egcd(a: i64, b: i64) -> (u64, u64, u64) {
+    let (mut prev_x, mut x) = (1, 0);
+    let (mut prev_y, mut y) = (0, 1);
+    let (mut prev_r, mut r) = (a, b);
+
+    while r != 0 {
+        let q = prev_r.div_euclid(r);
+        let temp = (r, x, y);
+        x = prev_x - q * x;
+        y = prev_y - q * y;
+        r = prev_r - q * r;
+        (prev_r, prev_x, prev_y) = temp;
+    }
+    (
+        prev_r.abs() as u64,
+        prev_x.rem_euclid(x.abs()) as u64,
+        prev_y.rem_euclid(y.abs()) as u64,
+    )
+}
+
+fn mod_inv(a: u64, b: u64) -> u64 {
+    let (gcd, c1, _) = egcd(a as i64, b as i64);
+    if gcd != 1 {
+        panic!("mod_inv({a}, {b}) not found");
+    }
+    c1.rem_euclid(b) as u64
 }
